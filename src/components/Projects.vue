b32abba916c8bec36a2412fc7daf7fb48682f981
arellak <nimble0@protonmail.com>
2024-01-07 19:04:27 +0100

    added more accessibility features, removed unsuded links

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,10 +3,10 @@
 
 <template>
 <div class="project-links">
-    <a href="https://github.com/arellak/elevenlabs-wrapper">ElevenLabs NodeJS</a>
-    <a href="https://github.com/arellak/mukaji_chat">Markov Chain Bot</a>
-    <a href="https://github.com/arellak/youtube-extension">YouTube Extension</a>
-    <a href="https://github.com/arellak/TerrariaClone">Terraria Raylib</a>
+    <a href="https://github.com/arellak/elevenlabs-wrapper" aria-label="ElevenLabs Wrapper in NodeJS">ElevenLabs NodeJS</a>
+    <a href="https://github.com/arellak/mukaji_chat" aria-label="Markov Chain Bot in NodeJS">Markov Chain Bot</a>
+    <a href="https://github.com/arellak/youtube-extension" aria-label="YouTube Extension to show extra useful information">YouTube Extension</a>
+    <a href="https://github.com/arellak/TerrariaClone" aria-label="Terraria Clone in RayLib and C++">Terraria Raylib</a>
 </div>
 </template>
 
