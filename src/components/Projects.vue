5802db7d1888e4e51a4c36f93c390917f7fbaab2
arellak <nimble0@protonmail.com>
2024-01-07 19:06:40 +0100

    removed/edited accessibility features

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,10 +3,10 @@
 
 <template>
 <div class="project-links">
-    <a href="https://github.com/arellak/elevenlabs-wrapper" aria-label="ElevenLabs Wrapper in NodeJS">ElevenLabs NodeJS</a>
-    <a href="https://github.com/arellak/mukaji_chat" aria-label="Markov Chain Bot in NodeJS">Markov Chain Bot</a>
-    <a href="https://github.com/arellak/youtube-extension" aria-label="YouTube Extension to show extra useful information">YouTube Extension</a>
-    <a href="https://github.com/arellak/TerrariaClone" aria-label="Terraria Clone in RayLib and C++">Terraria Raylib</a>
+    <a href="https://github.com/arellak/elevenlabs-wrapper" aria-label="ElevenLabs NodeJS">ElevenLabs NodeJS</a>
+    <a href="https://github.com/arellak/mukaji_chat" aria-label="Markov Chain Bot">Markov Chain Bot</a>
+    <a href="https://github.com/arellak/youtube-extension" aria-label="YouTube Extension">YouTube Extension</a>
+    <a href="https://github.com/arellak/TerrariaClone" aria-label="Terraria RayLib">Terraria Raylib</a>
 </div>
 </template>
 
